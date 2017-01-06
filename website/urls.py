f68cea2aa0384e263cd72bebcb4c8b86ba657f63
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2017-01-06 15:39:56 -0600

    .

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from Blogs import views
 
 urlpatterns = [
+    url(r"^$", views.homepage),
     url(r'^admin/', admin.site.urls),
 ]

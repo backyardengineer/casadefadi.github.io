61b10baa373dcc97eaaf48c7119ce2575fe77f5f
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2017-01-06 15:48:19 -0600

    .

--- Blogs/views.py
+++ Blogs/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def homepage(req):
     return render(req, "index.html")
+
+def aboutMe(req):
+    return render(req, "about-me.html")
+
+def blog(req):
+    return render(req, "blog.html")
+
+def elements(req):
+    return render(req, "elements.html")
--- website/urls.py
+++ website/urls.py
@@ -19,5 +19,8 @@ from Blogs import views
 
 urlpatterns = [
     url(r"^$", views.homepage),
+    url(r"^index.html", views.homepage),
+    url(r"^blog.html", views.blog),
+    url(r"^about-me.html", views.aboutMe),
     url(r'^admin/', admin.site.urls),
 ]

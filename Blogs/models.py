69cf12836bec177659658a37acdc7232259d346a
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2017-01-08 22:23:42 -0600

    .

--- Blogs/models.py
+++ Blogs/models.py
@@ -9,7 +9,7 @@ class BlogsModel(Model):
         verbose_name = "Blog"
         verbose_name_plural = "Blogs"
     title = CharField("Title", max_length=100, unique=True, blank=False, null=True)
-    content = TextField("Content", max_length=2000, blank=True, null=True)
+    content = TextField("Content", help_text="Word limit is 10000 characters", max_length=10000, blank=True, null=True)
     discoverable = BooleanField(help_text="Visible to public", default=True)
     publication_date = DateTimeField("Date published", default=timezone.now)
     _id = UUIDField("Blog ID(Sub-URL)", help_text="This entry is automatically generated", default=uuid4)
